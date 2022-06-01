4b72a4d0e214735c72ba50f0a16303a70bbbec1e
Mithrandiirr <swimox17@protonmail.com>
2022-06-01 21:26:35 +0100

    commit

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -10,7 +10,7 @@ function Features() {
   return (
     <div className='text-center  bg-dark pt-[7.75rem] pb-[11.625rem]'>
         <h3 className='font-bold text-[38.4px] tracking-[-0.64px]'>Features</h3>
-        <p className='mt-[5.75px] font-normal text-[16px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p className='mt-[5.75px] feature-p font-normal text-[16px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         <FeatureOne />
         <FeatureTwo />
         <FeatureThree />
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -15,8 +15,8 @@ function Landing() {
             <p className='landing-p mt-[2rem] w-[530px] leading-[27px] tracking-[0.471615px] text-center font-[16.76px]'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
             </p>
-            <div className='mt-[5rem] mb-[15.484375rem] '>
-                <button  type='button' className='px-[2.4rem] py-[0.836rem] font-bold text-[20.71px]  border-2 border-[#8480FF] rounded-[4.77487px]'>Learn More</button>
+            <div className='mt-[5rem] mb-[15.484375rem] btn '>
+                <button  type='button' className='px-[2.4rem] btn-btn py-[0.836rem] font-bold text-[20.71px]  border-2 border-[#8480FF] rounded-[4.77487px]'>Learn More</button>
             </div>
         </div>
     </>
